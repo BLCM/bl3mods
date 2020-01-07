a4d7ff42f71814696b58a20e242e1a422d74b5fe
CJ Kucera <pez@apocalyptech.com>
2020-01-06 23:56:12 -0600

    Oh right, was gonna buff the recharge rate even more

--- Apocalyptech/gen_transformer_super_buff.py
+++ Apocalyptech/gen_transformer_super_buff.py
@@ -25,7 +25,7 @@ for (attr, mod_type, mod_val) in [
         # Our buffs
         ('/Game/Gear/Shields/_Design/Balance/Attributes/Att_ShieldBalance_Capacity', 'ScaleSimple', 5000),
         ('/Game/Gear/Shields/_Design/Balance/Attributes/Att_ShieldBalance_RegenDelay', 'ScaleSimple', 0.0001),
-        ('/Game/Gear/Shields/_Design/Balance/Attributes/Att_ShieldBalance_RegenRate', 'ScaleSimple', 5000),
+        ('/Game/Gear/Shields/_Design/Balance/Attributes/Att_ShieldBalance_RegenRate', 'ScaleSimple', 50000),
 
         ]:
 
