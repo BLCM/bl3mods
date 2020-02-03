3fd0047d91745e5e53c02ea9729ffbc632ef5b09
CJ Kucera <pez@apocalyptech.com>
2020-02-03 14:52:06 -0600

    Tried to expand this to DLC1, which didn't actually work so well.  Will try again later.

--- Apocalyptech/gen_always_visible_challenge_icons.py
+++ Apocalyptech/gen_always_visible_challenge_icons.py
@@ -307,6 +307,45 @@ for (label, level_name, object_names) in sorted([
             '/Game/Maps/Zone_2/WetlandsVault/WetlandsVault_P.WetlandsVault_P:PersistentLevel.EridianWritingSpawner_.BP_ChallengeComponent_IconEridian',
             '/Game/Maps/Zone_2/WetlandsVault/WetlandsVault_P.WetlandsVault_P:PersistentLevel.BP_IO_Salvage_ClapTrapPart_AntennaAccessory.BP_CrewChallengeComponent_Salvage',
             ]),
+
+        # These mostly don't work yet, and have several downsides:
+        #
+        #   1) They activate the Torgue dialogues, but it turns out that those are always visible on the map anyway
+        #   2) They actively *prevent* the other sorts from ever showing up.  You can still *do* the challenges if
+        #      you go there, but the dialogue never starts and you never get credit for it.
+        #   3) They seem to prevent the loader spawns right outside Timothy's hideout.  (Perhaps that's all the
+        #      `Strip_Central_TimLair_CrewIOStatus` stuff, actually?)
+        #
+        # So anyway, just commenting for now.  Will try to piece that together later.
+
+        #("Grand Opening", 'CasinoIntro_P', [
+        #    ]),
+        #("Jack's Secret", 'Core_P', [
+        #    '/Dandelion/Maps/Core/Core_Gameplay.Core_Gameplay:PersistentLevel.IO_CrewChallenge_Sabotage_DLC_7.BP_CrewChallengeComp_Sabotage_DLC1',
+        #    '/Dandelion/Maps/Core/Core_Gameplay.Core_Gameplay:PersistentLevel.IO_CrewChallenge_Collect_DLC_5.BP_CrewChallengeComponent_Collection',
+        #    ]),
+        #("Impound Deluxe", 'Impound_P', [
+        #    '/Dandelion/Maps/Impound/Impound_Gameplay.Impound_Gameplay:PersistentLevel.IO_CrewChallenge_Kill_DLC_2.BP_CrewChallengeComp_KillTarget_DLC1',
+        #    '/Dandelion/Maps/Impound/Impound_Gameplay.Impound_Gameplay:PersistentLevel.IO_CrewChallenge_Sabotage_DLC_2.BP_CrewChallengeComp_Sabotage_DLC1',
+        #    ]),
+        #("Spendopticon", 'Strip_P', [
+        #    '/Dandelion/Maps/Strip/Strip_Interactives.Strip_Interactives:PersistentLevel.IO_CrewChallenge_Collect_DLC_4.BP_CrewChallengeComponent_Collection',
+        #    '/Dandelion/Maps/Strip/Strip_PM_MeetCrad.Strip_PM_MeetCrad:PersistentLevel.IO_CrewChallenge_Sabotage_DLC_2.BP_CrewChallengeComp_Sabotage_DLC1',
+        #    '/Dandelion/Maps/Strip/Strip_Central_TimLair_CrewIOStatus.Strip_Central_TimLair_CrewIOStatus:PersistentLevel.IO_CrewChallenge_Sabotage_DLC1_Strip__0.BP_CrewChallengeComp_Sabotage_DLC1',
+        #    '/Dandelion/Maps/Strip/Strip_Central_TimLair_CrewIOStatus.Strip_Central_TimLair_CrewIOStatus:PersistentLevel.IO_CrewChallenge_Sabotage_DLC1_Strip_.BP_CrewChallengeComp_Sabotage_DLC1',
+        #    '/Dandelion/Maps/Strip/Strip_Central_TimLair_CrewIOStatus.Strip_Central_TimLair_CrewIOStatus:PersistentLevel.IO_CrewChallenge_Kill_DLC1__0.BP_CrewChallengeComp_KillTarget_DLC1',
+        #    '/Dandelion/Maps/Strip/Strip_Central_TimLair_CrewIOStatus.Strip_Central_TimLair_CrewIOStatus:PersistentLevel.IO_CrewChallenge_Kill_DLC1_.BP_CrewChallengeComp_KillTarget_DLC1',
+        #    '/Dandelion/Maps/Strip/Strip_Central_TimLair_CrewIOStatus.Strip_Central_TimLair_CrewIOStatus:PersistentLevel.IO_CrewChallenge_Collect_DLC_2.BP_CrewChallengeComponent_Collection',
+        #    ]),
+        #("VIP Tower", 'TowerLair_P', [
+        #    '/Dandelion/Maps/TowerLair/TowerLair_Interactives.TowerLair_Interactives:PersistentLevel.IO_CrewChallenge_Kill_DLC_2.BP_CrewChallengeComp_KillTarget_DLC1',
+        #    '/Dandelion/Maps/TowerLair/TowerLair_Interactives.TowerLair_Interactives:PersistentLevel.IO_CrewChallenge_Sabotage_DLC_2.BP_CrewChallengeComp_Sabotage_DLC1',
+        #    '/Dandelion/Maps/TowerLair/TowerLair_Interactives.TowerLair_Interactives:PersistentLevel.IO_CrewChallenge_Collect_DLC_2.BP_CrewChallengeComponent_Collection',
+        #    ]),
+        #("Compactor", 'Trashtown_P', [
+        #    '/Dandelion/Maps/Trashtown/Trashtown_Gameplay.Trashtown_Gameplay:PersistentLevel.IO_CrewChallenge_Kill_DLC_5.BP_CrewChallengeComp_KillTarget_DLC1',
+        #    '/Dandelion/Maps/Trashtown/Trashtown_Gameplay.Trashtown_Gameplay:PersistentLevel.IO_CrewChallenge_Collect_DLC_2.BP_CrewChallengeComponent_Collection',
+        #    ]),
         ]):
 
     mod.comment(label)
