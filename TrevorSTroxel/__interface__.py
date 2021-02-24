1ebcaaaaf2de71a0870cf02f874408c6084ccb9f
TrevorSTroxel <ttroxel21@my.whitworth.edu>
2021-02-23 20:59:00 -0800

    Some function inheratince

--- TrevorSTroxel/__interface__.py
+++ TrevorSTroxel/__interface__.py
@@ -1,6 +1,6 @@
 import tkinter as tk 
 from tkinter import *
-import __Generate_Hotfix
+from __Generate_Hotfix import test1, test2, test3, test4
 #reference: https://www.geeksforgeeks.org/python-gui-tkinter/
 
 if __name__ == "__main__":
@@ -8,9 +8,9 @@ if __name__ == "__main__":
     window.title("Hot Fix Generator")
     window.geometry("500x500")
     text1 = Button(text="1. Mod Header", font=("Times New Roman", 18), command = test1)
-    text2 = Button(text="2. Choose File", font=("Times New Roman", 18))
-    text3 = Button(text="3. Find All References", font=("Times New Roman", 18))
-    text4 = Button(text="4. Make Regular Hotfix", font=("Times New Roman", 18))
+    text2 = Button(text="2. Choose File", font=("Times New Roman", 18), command = test1)
+    text3 = Button(text="3. Find All References", font=("Times New Roman", 18), command = test1)
+    text4 = Button(text="4. Make Regular Hotfix", font=("Times New Roman", 18), command = test1)
 
     #this will pack everything so that I do not have to do it every time
     for c in sorted(window.children):
