b44c5a2e1446e35a4bc06e820b0e6ad75b3215e3
TrevorSTroxel <ttroxel21@my.whitworth.edu>
2021-02-23 20:54:51 -0800

    Momre functionality

--- TrevorSTroxel/__interface__.py
+++ TrevorSTroxel/__interface__.py
@@ -1,11 +1,13 @@
 import tkinter as tk 
 from tkinter import *
+import __Generate_Hotfix
+#reference: https://www.geeksforgeeks.org/python-gui-tkinter/
 
 if __name__ == "__main__":
     window = tk.Tk()
     window.title("Hot Fix Generator")
     window.geometry("500x500")
-    text1 = Button(text="1. Mod Header", font=("Times New Roman", 18))
+    text1 = Button(text="1. Mod Header", font=("Times New Roman", 18), command = test1)
     text2 = Button(text="2. Choose File", font=("Times New Roman", 18))
     text3 = Button(text="3. Find All References", font=("Times New Roman", 18))
     text4 = Button(text="4. Make Regular Hotfix", font=("Times New Roman", 18))
