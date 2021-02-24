2763861e1bf35cffc6f94fd53272ca7b5bdf09c3
TrevorSTroxel <ttroxel21@my.whitworth.edu>
2021-02-23 21:17:09 -0800

    buttons work

--- TrevorSTroxel/__interface__.py
+++ TrevorSTroxel/__interface__.py
@@ -8,9 +8,9 @@ if __name__ == "__main__":
     window.title("Hot Fix Generator")
     window.geometry("500x500")
     text1 = Button(text="1. Mod Header", font=("Times New Roman", 18), command = test1)
-    text2 = Button(text="2. Choose File", font=("Times New Roman", 18), command = test1)
-    text3 = Button(text="3. Find All References", font=("Times New Roman", 18), command = test1)
-    text4 = Button(text="4. Make Regular Hotfix", font=("Times New Roman", 18), command = test1)
+    text2 = Button(text="2. Choose File", font=("Times New Roman", 18), command = test2)
+    text3 = Button(text="3. Find All References", font=("Times New Roman", 18), command = test3)
+    text4 = Button(text="4. Make Regular Hotfix", font=("Times New Roman", 18), command = test4)
 
     #this will pack everything so that I do not have to do it every time
     for c in sorted(window.children):
