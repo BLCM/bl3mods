201a039fcdac4be77d37c7e4b7bc285a5fe4baab
TrevorSTroxel <ttroxel21@my.whitworth.edu>
2021-02-24 21:13:36 -0800

    Worked on UI and planned out look

--- TrevorSTroxel/__interface__.py
+++ TrevorSTroxel/__interface__.py
@@ -4,8 +4,7 @@ from __Generate_Hotfix import test1, test2, test3, test4
 #reference: https://www.geeksforgeeks.org/python-gui-tkinter/
 
 #Creates a new window for the user to see and for the commands to be used
-
-def newwindow(func):
+def NeWindow(func):
     Nwindow = Tk()
     #generics we can reuse for any task I have created
     entry1=StringVar(Nwindow)
@@ -20,9 +19,16 @@ def newwindow(func):
         if func == test1:
             a,b,c,d,e,f = entry1.get(), entry2.get(), entry3.get(), entry4.get(), entry5.get(), entry6.get()
             test1(a,b,c,d,e,f)
+            #this will be used to display what is going to put in the hotfix file
+        elif func == test2:
+            None
+        elif func == test3:
+            None
+        elif func == test4:
+            None
+        
     
-    
-    if func == test1:
+    if func == test1: #creates a mod file of you to use
         Nwindow.title("Mod header")
         Label(Nwindow, text= "Name of the hotfix file: ").grid(row=0)
         Entry(Nwindow, textvariable = entry1).grid(row = 0, column = 1)
@@ -44,18 +50,16 @@ def newwindow(func):
     elif func == test4:
         None
     
-
-
 if __name__ == "__main__":
     window = tk.Tk()
     window.title("Hot Fix Generator")
     # window.geometry("500x500")
-    Button(text="1. Mod Header", font=("Times New Roman", 18), command = lambda: newwindow(test1))
-    Button(text="2. Choose File", font=("Times New Roman", 18), command = lambda: newwindow(test2))
-    Button(text="3. Find All References", font=("Times New Roman", 18), command = lambda: newwindow(test3))
-    Button(text="4. Make Regular Hotfix", font=("Times New Roman", 18), command = lambda: newwindow(test4))
+    Button(text="1. Mod Header", font=("Times New Roman", 18), command = lambda: NeWindow(test1))
+    Button(text="2. Choose File", font=("Times New Roman", 18), command = lambda: NeWindow(test2))
+    Button(text="3. Find All References", font=("Times New Roman", 18), command = lambda: NeWindow(test3))
+    Button(text="4. Make Regular Hotfix", font=("Times New Roman", 18), command = lambda: NeWindow(test4))
 
     #this will pack everything so that I do not have to do it every time
     for c in sorted(window.children):
-        window.children[c].pack(side="top")
+        window.children[c].pack()
     window.mainloop()
