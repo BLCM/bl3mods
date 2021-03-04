a5a2aa63d7d92bc6ac8cd5f31c7d9b2c1678bfe8
TrevorSTroxel <ttroxel21@my.whitworth.edu>
2021-03-03 22:50:48 -0800

    Cleaning up code

--- python_mod_helpers/hotfixgenerator/__interface__.py
+++ python_mod_helpers/hotfixgenerator/__interface__.py
@@ -1,28 +1,10 @@
-from __Generate_Hotfix import JSONInfo, Search, Create_HotFix_File
+from __Generate_Hotfix import JSONInfo, Search, Create_HotFix_File, FileChoice
 from bl3data import BL3Data
-from _global_lists import FileNames, Mod_Header, Reg_hotfix, Information
-import os
+from _global_lists import Mod_Header, Reg_hotfix, List_Info
 import tkinter as tk
 from tkinter import *
-from tkinter.filedialog import askopenfilename
 data = BL3Data()
 
-#This is used as a referencesto look and see what are the names of the game folders
-#the user is able to choose a json file
-def FileChoice():
-    file = askopenfilename(filetypes=[("Choose file", ".json")])
-    # Removes the files extention, as we dont need it
-    raw_path = os.path.splitext(file)[0]
-    i = 0
-    index = 0
-    while index <= 0:
-        Find = "/" + FileNames[i]
-        i += 1
-        if Find in raw_path:
-            index = raw_path.find(Find)
-    # this is the data we need to pass in information
-    True_Path = raw_path[index::]
-    JSONInfo(True_Path)
 
 #Creates a new window for the user to see and for the commands to be used
 def NeWindow(func):
@@ -30,25 +12,20 @@ def NeWindow(func):
     #these variables will determine how many things to add to the window as i need them
     l, e, b = 0, 0, 0
     #generics we can reuse for any task I have created
-    entry1 = StringVar(Nwindow)
-    entry2 = StringVar(Nwindow)
-    entry3 = StringVar(Nwindow)
-    entry4 = StringVar(Nwindow)
-    entry5 = StringVar(Nwindow)
-    entry6 = StringVar(Nwindow)
+    entry1, entry2, entry3, entry4, entry5, entry6 = StringVar(Nwindow), StringVar(Nwindow), StringVar(Nwindow), StringVar(Nwindow), StringVar(Nwindow), StringVar(Nwindow)
     
     # Used to grab the values the then entry textvariables, 
     # So that for any function I can call them and reuse them
-    def get_val(type):
+    def Get_Val(type):
         if type == "ModHeader":
             a, b, c, d, e, f = entry1.get(), entry2.get(), entry3.get(), entry4.get(), entry5.get(), entry6.get()
-            Mod_Header.extend([a, b, c, d, e, f])
+            Mod_Header.extend([a, b, c, d, e, f]) # Puts the Information to make the file into a queue to be called later
+        elif type == "HotFix":
+            a, b, c, d, e, f = entry1.get(), entry2.get(), entry3.get(), entry4.get(), entry5.get(), entry6.get()
+            Reg_hotfix.extend([a, b, c, d, e, f]) #info is put into a regular hotfix queue for later
         elif type == Search:
             a = entry1.get()
             Search(a)
-        elif type == "HotFix":
-            a, b, c, d, e, f = entry1.get(), entry2.get(), entry3.get(), entry4.get(), entry5.get(), entry6.get()
-            Reg_hotfix.extend([a, b, c, d, e, f])
 
     #So now this works for the most part. the inner code still needs to work
     if func == "ModHeader":  # creates a mod file of you to use
@@ -59,10 +36,11 @@ def NeWindow(func):
         text4 = 'Discription: '
         text5 = 'Version of this mod: '
         text6 = 'The catagory in which this mods fits to: '
-        b1text = 'Create Mod Header'
         l = 6
         e = 6
-        def b1command(): return get_val("ModHeader")
+
+        b1text = 'Create Mod Header'
+        def b1command(): return Get_Val("ModHeader")
         b = 1
     
     elif func == "HotFix":
@@ -75,30 +53,21 @@ def NeWindow(func):
         text6 = 'For right now type "", will give better instructions later when I understand it more'
         l = 6
         e = 6
+
         b1text = "Add This Regular Hotfix To The Queue"
-        def b1command(): return get_val("HotFix")
+        def b1command(): return Get_Val("HotFix")
         b = 1
 
     # Come back to this function later, as it is giving me to much grief right now
-    elif func == JSONInfo:  # this will be more fleshed out later, but for now it I am trying to get the interfce to work
-        Nwindow.title("Look Through File information")
-        l = 0
-        e = 0   
-
-        b1text = "Choose file"
-        def b1command(): return FileChoice()
-        b = 1
-
     elif func == Search:
         Nwindow.title("Find All References")
-        text1 = 'Enter what you want to search for: '
+        text1 = 'Enter what you want to search for: \n NOTE: To look at your search results, please click the 5th button and go from there. \n NOTE: Sometimes the program will take a while to load in all the data, so be patient with it.'
         l = 1
         e = 1 
 
         b1text = "Search"
-        def b1command(): return get_val(Search)
+        def b1command(): return Get_Val(Search)
         b = 1
-
 
     # This is the best way for the program to reuse generics, 
     # While being able to determine how many are needed for a paticular program
@@ -142,10 +111,10 @@ if __name__ == "__main__":
     window.title("Hot Fix Generator")
     # window.geometry("500x500")
     Button(text="1. Add To Mod Header Queue", font=("Times New Roman", 18), command=lambda: NeWindow("ModHeader"))
-    Button(text="2. Choose File To Search Through", font=("Times New Roman", 18), command=lambda: NeWindow(JSONInfo), state=DISABLED) #Will reenable later once i can get it working
+    Button(text="2. Choose File To Search Through", font=("Times New Roman", 18), command=lambda: FileChoice()) #Will reenable later once i can get it working
     Button(text="3. Find All References To An Object", font=( "Times New Roman", 18), command=lambda: NeWindow(Search))
-    Button(text="4. Add To The HotFix", font=( "Times New Roman", 18), command=lambda: NeWindow("HotFix"))
-    Button(text="5. Click to look at the Stored information that might be helpful to you", font=( "Times New Roman", 18), command=lambda: Information())
+    Button(text="4. Add To The HotFix Queue", font=( "Times New Roman", 18), command=lambda: NeWindow("HotFix"))
+    Button(text="5. Click To Look At Stored information", font=( "Times New Roman", 18), command=lambda: List_Info())
     Button(text="Click This To Create Your HotFix File", font=( "Times New Roman", 18), command=lambda: Create_HotFix_File())
     #this will pack everything so that I do not have to do it every time
     for c in sorted(window.children):
