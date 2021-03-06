5afc225cf4ff924b4274fe6a5043566861001c1f
Abram Hindle <abram.hindle@softwareprocess.es>
2021-03-06 14:09:11 -0700

    [RAID] Refactored raid.py to be more consistent and have optional overrides.

--- skruntskrunt/raid/raid.py
+++ skruntskrunt/raid/raid.py
@@ -12,6 +12,7 @@ DEFAULT_HEALTH=1000
 DEFAULT_NLOOT=12
 
 def buff_boss(boss): #bpchar, bpchar_path, balance_table, rowname, health,damage,nloot
+    """ Generate hotfix code for buffing a boss """
     out = []
     out.append(f"##### {boss['name']} #####")
     out.append(f"# BPChar: {boss['bpchar']}")
@@ -25,71 +26,59 @@ def buff_boss(boss): #bpchar, bpchar_path, balance_table, rowname, health,damage
     out.append(f"SparkCharacterLoadedEntry,(1,2,0,{boss['bpchar']}),{boss['balance_table']}.{basename(boss['balance_table'])},{boss['rowname']},{DAMAGE},0,,{boss['damage']}")
     return "\n".join(out)
 
-tyreen = {
-    'name':'Tyreen',
-    'bpchar':'BPChar_FinalBoss',
-    'bpchar_path':'/Game/Enemies/FinalBoss/_Shared/_Design/Character/BPChar_FinalBoss',
-    'rowname':'FinalBoss',
-    'balance_table':'/Game/Enemies/FinalBoss/_Shared/_Design/Balance/Table_Balance_FinalBoss_PT1',
-    'nloot':DEFAULT_NLOOT,
-    'health':[DEFAULT_HEALTH,DEFAULT_HEALTH],
-    'damage':DEFAULT_DAMAGE
-}
-troy = {
-    'name':'Troy',
-    'bpchar':'BPChar_TroyBoss',
-    'bpchar_path':'/Game/NonPlayerCharacters/Troy/_TheBoss/_Design/Character/BPChar_TroyBoss',
-    'rowname':'TroyBoss',
-    'balance_table':'/Game/NonPlayerCharacters/Troy/_Design/Balance/Table_Balance_TroyBoss_PT1',
-    'nloot':DEFAULT_NLOOT,
-    'health':[DEFAULT_HEALTH,DEFAULT_HEALTH],
-    'damage':DEFAULT_DAMAGE
-}
-rampager = {
-    'name':'Rampager',
-    'bpchar':'BPChar_Rampager',
-    'bpchar_path':'/Game/Enemies/PrometheaBoss/Rampager/_Design/Character/BPChar_Rampager',
-    'rowname':'Rampager',
-    'balance_table':'/Game/Enemies/PrometheaBoss/_Shared/_Design/Balance/Table_Balance_PromBoss_PT1',
-    'nloot':DEFAULT_NLOOT,
-    'health':[4*DEFAULT_HEALTH,4*DEFAULT_HEALTH],
-    'damage':DEFAULT_DAMAGE    
-}
-def mk_boss(name, bpchar_path, balance_table, rowname):
+def mk_boss(name, bpchar_path, balance_table, rowname,options=None):
+    """ factory for boss definitions (hint: shoulda been a class) """
+    if options is None:
+        options = dict()
     return {
         'name':name,
         'bpchar':basename(bpchar_path),
         'bpchar_path':bpchar_path,
         'rowname':rowname,
         'balance_table':balance_table,
-        'nloot':DEFAULT_NLOOT,
-        'health':[DEFAULT_HEALTH,DEFAULT_HEALTH,DEFAULT_HEALTH,DEFAULT_HEALTH,DEFAULT_HEALTH],
-        'damage':DEFAULT_DAMAGE    
+        'nloot':options.get('nloot',DEFAULT_NLOOT),
+        'health':options.get('health',[DEFAULT_HEALTH,DEFAULT_HEALTH,DEFAULT_HEALTH,DEFAULT_HEALTH,DEFAULT_HEALTH]),
+        'damage':options.get('damage',DEFAULT_DAMAGE),    
     }
 
-ruiner = mk_boss('Ruiner', '/Geranium/Enemies/Ruiner/Boss/_Design/Character/BPChar_RuinerBoss', '/Geranium/Enemies/Ruiner/_Shared/_Design/Balance/Table_Ruiner_Balance', 'Ruiner_Boss_PT2')
-ruiner["health"] = [x*3 for x in ruiner["health"]]
-ruiner["damage"] = 50
-minosaur = mk_boss('Minosaur','/Geranium/Enemies/GerSaurian/_Unique/Saurtaur/_Design/Character/BPChar_GerSaurianSaurtaur','/Geranium/Enemies/GerSaurian/_Shared/_Design/Balance/Table_GerSaurian_Balance_Unique','GerSaurian_Saurtaur')
-
-dumptruck = mk_boss('Dumptruck',
-                    '/Game/Enemies/Enforcer/_Unique/BountyPrologue/_Design/Character/BPChar_Enforcer_BountyPrologue',
-                    '/Game/Enemies/Enforcer/_Shared/_Design/Balance/Table_Enforcer_Balance_Unique',
-                    'Enforcer_BountyPrologue')
-mouthpiece = mk_boss('MouthPiece',
-                     '/Game/Enemies/Enforcer/_Unique/SacrificeBoss/_Design/Character/BPChar_EnforcerSacrificeBoss',
-                     '/Game/Enemies/Enforcer/_Shared/_Design/Balance/Table_Enforcer_Balance_Unique',
-                     'Enforcer_Mouthpiece')
-hotkarl = mk_boss('HotKarl',
-                     '/Game/Enemies/Enforcer/_Unique/Bounty01/_Design/Character/BPChar_Enforcer_Bounty01',
-                     '/Game/Enemies/Enforcer/_Shared/_Design/Balance/Table_Enforcer_Balance_Unique',
-                     'Enforcer_Bounty01_HotKarl')
-shiv = mk_boss('Shiv',
-               '/Game/Enemies/Psycho_Male/_Unique/BadassPrologue/_Design/Character/BPChar_PsychoBadassPrologue',
-               "/Game/Enemies/Psycho_Male/_Shared/_Design/Balance/Table_Psycho_Balance",
-               "Psycho_Badass")
 # name, BP Path, Balance Table, Row Key in Balance Table
 more_bosses = [
+    ('Tyreen',
+     '/Game/Enemies/FinalBoss/_Shared/_Design/Character/BPChar_FinalBoss',
+     '/Game/Enemies/FinalBoss/_Shared/_Design/Balance/Table_Balance_FinalBoss_PT1',
+     'FinalBoss'),
+    ('Troy',
+     '/Game/NonPlayerCharacters/Troy/_TheBoss/_Design/Character/BPChar_TroyBoss',
+     '/Game/NonPlayerCharacters/Troy/_Design/Balance/Table_Balance_TroyBoss_PT1',
+     'TroyBoss'),
+
+    ('Rampager','/Game/Enemies/PrometheaBoss/Rampager/_Design/Character/BPChar_Rampager',
+     '/Game/Enemies/PrometheaBoss/_Shared/_Design/Balance/Table_Balance_PromBoss_PT1',
+     'Rampager',
+     {
+         'nloot':DEFAULT_NLOOT,
+         'health':[4*DEFAULT_HEALTH,4*DEFAULT_HEALTH],
+         'damage':DEFAULT_DAMAGE
+     }
+    ),
+    ('Ruiner', '/Geranium/Enemies/Ruiner/Boss/_Design/Character/BPChar_RuinerBoss', '/Geranium/Enemies/Ruiner/_Shared/_Design/Balance/Table_Ruiner_Balance', 'Ruiner_Boss_PT2',{"health":[DEFAULT_HEALTH*3 for health in HEALTHS],"damage":50,"nloot":20}),
+    ('Minosaur','/Geranium/Enemies/GerSaurian/_Unique/Saurtaur/_Design/Character/BPChar_GerSaurianSaurtaur','/Geranium/Enemies/GerSaurian/_Shared/_Design/Balance/Table_GerSaurian_Balance_Unique','GerSaurian_Saurtaur'),
+    ('Dumptruck',
+     '/Game/Enemies/Enforcer/_Unique/BountyPrologue/_Design/Character/BPChar_Enforcer_BountyPrologue',
+     '/Game/Enemies/Enforcer/_Shared/_Design/Balance/Table_Enforcer_Balance_Unique',
+     'Enforcer_BountyPrologue'),
+    ('MouthPiece',
+     '/Game/Enemies/Enforcer/_Unique/SacrificeBoss/_Design/Character/BPChar_EnforcerSacrificeBoss',
+     '/Game/Enemies/Enforcer/_Shared/_Design/Balance/Table_Enforcer_Balance_Unique',
+     'Enforcer_Mouthpiece'),
+    ('HotKarl',
+     '/Game/Enemies/Enforcer/_Unique/Bounty01/_Design/Character/BPChar_Enforcer_Bounty01',
+     '/Game/Enemies/Enforcer/_Shared/_Design/Balance/Table_Enforcer_Balance_Unique',
+     'Enforcer_Bounty01_HotKarl'),
+    ('Shiv',
+     '/Game/Enemies/Psycho_Male/_Unique/BadassPrologue/_Design/Character/BPChar_PsychoBadassPrologue',
+     "/Game/Enemies/Psycho_Male/_Shared/_Design/Balance/Table_Psycho_Balance",
+     "Psycho_Badass"),
     ('RoadDog','/Game/Enemies/Goliath/_Unique/Rare02/_Design/Character/BPChar_Goliath_Rare02','/Game/Enemies/Goliath/_Shared/_Design/Balance/Table_Balance_Goliath_Unique','Rare02'),
     ('Princess Tarantella II','/Game/Enemies/Spiderant/_Unique/Tarantella/_Design/Character/BPChar_SpiderantTarantella','/Game/Enemies/Spiderant/_Shared/_Design/Balance/Table_Balance_Spiderant_Unique','Spiderant_CakeRoyalty'),
     ('Waylon Hurd','/Geranium/Enemies/GerPsycho_Male/_Unique/MoleMan/_Design/Character/BPChar_GerPsychoMoleMan','/Geranium/Enemies/GerPsycho_Male/_Shared/_Design/Balance/Table_GerPsycho_Balance_Unique','GerPsycho_MoleMan'),
@@ -114,14 +103,22 @@ more_bosses = [
     ('IndoTyrant','/Game/Enemies/Saurian/_Unique/Rare01/_Design/Character/BPChar_Saurian_Rare01',"/Game/Enemies/Saurian/_Shared/_Design/Balance/Table_Balance_Saurian_Unique","Saurian_Rare01"),
     ('Demoskaggon','/Game/Enemies/Skag/_Unique/Rare01/_Design/Character/BPChar_Skag_Rare01','/Game/Enemies/Skag/_Shared/_Design/Balance/Table_Skag_Balance_Unique',"DemoSkag"),
     ('Warden','/Game/Enemies/Goliath/_Unique/CageArena/_Design/Character/BPChar_Goliath_CageArena',"/Game/Enemies/Goliath/_Shared/_Design/Balance/Table_Balance_Goliath_Unique","CageArena"),
-    ('Agonizer 9000','/Game/Enemies/Agonizer_9k/_Shared/Character/BPChar_Agonizer_9k',"/Game/Enemies/Agonizer_9k/_Shared/Balance/Table_Balance_Agonizer_9k","A9K")    
+    ('Agonizer 9000','/Game/Enemies/Agonizer_9k/_Shared/Character/BPChar_Agonizer_9k',"/Game/Enemies/Agonizer_9k/_Shared/Balance/Table_Balance_Agonizer_9k","A9K"),
+    ('Wick','/Game/Enemies/Psycho_Male/_Unique/Rare03/_Design/Character/BPChar_PsychoRare03','/Game/Enemies/Psycho_Male/_Shared/_Design/Balance/Table_Psycho_Balance_Unique','Rare03'),
+    ('Borman Nates','/Game/Enemies/Psycho_Male/_Unique/Rare03/_Design/Character/BPChar_PsychoRare02','/Game/Enemies/Psycho_Male/_Shared/_Design/Balance/Table_Psycho_Balance_Unique','Rare02'),
+    ('Warty','/Game/Enemies/Psycho_Male/_Unique/Rare03/_Design/Character/BPChar_PsychoRare01','/Game/Enemies/Psycho_Male/_Shared/_Design/Balance/Table_Psycho_Balance_Unique','Rare01'),
+    ('OnePunch','/Game/Enemies/Psycho_Male/_Unique/Rare03/_Design/Character/BPChar_PsychoRare03','/Game/Enemies/Psycho_Male/_Shared/_Design/Balance/Table_Psycho_Balance_Unique','OnePunch'),
+    # ('Tumorhead','/Game/Enemies/Punk_Female/_Unique/TumorHead/_Design/Character/BPChar_PunkBadass_Tumorhead',None,None), #punk_badass non-unique
+    ('Judge Hightower','/Game/NonPlayerCharacters/_Promethea/AtlasSoldier/_Design/Character/BPChar_AtlasSoldier_Bounty01','/Game/NonPlayerCharacters/_Shared/_Design/Table_Balance_NPC','AtlasSoldier_Bounty'),
+    ('Urist McEnforcer','/Game/Enemies/Enforcer/_Unique/Urist/_Design/Character/BPChar_EnforcerUrist','/Game/Enemies/Enforcer/_Shared/_Design/Balance/Table_Enforcer_Balance','Enforcer_Shield'), # I hope this is safe
+    ('Killavolt (Kenneth)','/Game/Enemies/Enforcer/_Unique/KillaVolt/_Design/Character/BPChar_EnforcerKillaVolt','/Game/Enemies/Enforcer/_Shared/_Design/Balance/Table_Enforcer_Balance_Unique','Enforcer_KillaVolt'),
+    # Katagawa Jr has holograms too, row KatagawaJR_Hologram
+    ('Katagawa Jr.','/Game/Enemies/KatagawaJR/KJR/_Design/Character/BPChar_KJR','/Game/Enemies/KatagawaJR/_Shared/_Design/Balance/Table_Balance_KatagawaJR_PT1','KatagawaJR_Boss'),
+    # Make the holograms kinda wimpy
+    ('Katagawa Jr (Hologram).','/Game/Enemies/KatagawaJR/KJR/_Design/Character/BPChar_KJR','/Game/Enemies/KatagawaJR/_Shared/_Design/Balance/Table_Balance_KatagawaJR_PT1','KatagawaJR_Hologram',{'health':[DEFAULT_HEALTH/4]}),
 ]
 
-
-bosses = [tyreen, troy, rampager, ruiner, minosaur,
-          dumptruck, mouthpiece, hotkarl, shiv] 
-more_bosses = [mk_boss(*x) for x in more_bosses]
-bosses = bosses + more_bosses
+bosses = [mk_boss(*x) for x in more_bosses]
 
 for boss in bosses:
     print(buff_boss(boss))
