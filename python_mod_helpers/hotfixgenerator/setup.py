d1e7ef7809599476673ecbfe4a0aedfeb02bcfcf
TrevorSTroxel <ttroxel21@my.whitworth.edu>
2021-03-21 21:15:45 -0700

    Updated design and testing things

--- python_mod_helpers/hotfixgenerator/setup.py
+++ python_mod_helpers/hotfixgenerator/setup.py
@@ -1,4 +1,17 @@
 from distutils.core import setup
 import py2exe
+import os
+from bl3data import BL3Data
+from __info_function__ import FileChoice, openBL3Hotfixfile
+from __hotfix_control import Create_HotFix_File
+from _global_lists import *
+################################################################################################################################################################
+# Libraies
+import tkinter as tk
+from tkinter import *
+from bl3data import BL3Data
+from _global_lists import *
+################################################################################################################################################################
+from flatten_json import flatten
 
-setup(console=['__interface__.py'])
+setup(windows=['__interface__.py'])
