b111ab0af62c2423c5f94457bf0dd284eba102ec
CJ Kucera <pez@apocalyptech.com>
2021-04-12 00:11:44 -0500

    Releasing Better Maliwan Charge Time v1.3.0

--- Apocalyptech/gear_changes/better_maliwan_charge_time/README.md
+++ Apocalyptech/gear_changes/better_maliwan_charge_time/README.md
@@ -1,28 +1,30 @@
 Better Maliwan Charge Time
 ==========================
 
-Improves the charge time of all Maliwan weapons except for the following:
-
-* Starkiller
-* Ice Pick
-* D.N.A.
-* Atomizer pistols
-* Melter pistols
+Improves the charge time of all Maliwan weapons.
 
 Pistols and SMGs receive a 50% improvement, shotguns get 60%, and snipers will always
 be instant-fire (though the models which fire on *releasing* the trigger will still do so).
 
-This has been updated through DLC4 (Psycho Krieg and the Fantastic Fustercluck).
+This has been updated through DLC6 (Director's Cut).
 
 TODO
 ====
 
-- Would love to figure out those left-out guns
 - It might make sense to also include a few non-Maliwan guns which have similar charging
   mechanics
 
 Changelog
 =========
+
+**v1.3.0** - Apr 12, 2020
+ * Updated for DLC6 (Director's Cut), which is just Free Radical
+ * Added the following weapons I hadn't figured out in earlier versions:
+   * D.N.A.
+   * Starkiller
+   * Ice Pick
+   * Atomizer pistols
+   * Melter pistols
 
 **v1.2.0** - Nov 10, 2020
  * Updated for DLC5 (Designer's Cut) weapons (Binary Operator and Superconducting
--- Apocalyptech/gear_changes/better_maliwan_charge_time/gen_better_maliwan_charge_time.py
+++ Apocalyptech/gear_changes/better_maliwan_charge_time/gen_better_maliwan_charge_time.py
@@ -33,24 +33,21 @@ mod = Mod('better_maliwan_charge_time.bl3hotfix',
         'Better Maliwan Charge Time',
         'Apocalyptech',
         [
-            'Reduces the charge time for most Maliwan guns.  Notable exceptions at the moment',
-            'are mostly pistols: the unique guns Starkiller, Ice Pick, and D.N.A.; and the',
-            'generic Maliwan Atomizers and Melters.  Those seem to use unique timing',
-            'mechanisms which nothing else uses.',
+            'Reduces the charge time for Maliwan guns.',
             '',
             "Note that it's possible that any gun whose damage depends on charge time could",
             '*possibly* be nerfed by this, if the damage is based on time and not charge',
             "percent.  I think they're probably fine but have not tested it.",
             '',
-            'Pistol Scaling: {:d}% (except Starkiller, Ice Pick, Atomizers, and Melters)'.format(int(scale_pistol*100)),
+            'Pistol Scaling: {:d}%'.format(int(scale_pistol*100)),
             'Shotgun Scaling: {:d}%'.format(int(scale_shotgun*100)),
-            'SMG Scaling: {:d}% (except D.N.A.)'.format(int(scale_smg*100)),
+            'SMG Scaling: {:d}%'.format(int(scale_smg*100)),
             'Sniper Scaling: {:d}%'.format(int(scale_sniper*100)),
             #'',
             #"Also (possibly) buffs the charge time for the ION CANNON, even though it's a Vladof gun.",
         ],
         lic=Mod.CC_BY_SA_40,
-        v='1.2.0',
+        v='1.3.0',
         cats='gear-brand, cheat',
         )
 
@@ -73,72 +70,91 @@ def charge_time(mod,
             '(BaseValue={:.6f})'.format(new_val),
             prev_val=prev_val)
 
-# TODO: Blind Sage - /Game/PatchDLC/Alisma/Gear/Weapon/_Unique/BlindBandit/Balance/Balance_SG_MAL_BlindBandit
-# (and non-legendary equivalent)
-
 # Put in our improvements
 mod.header('Generic Weapons')
-for (label, obj_name, aspect_num, default, scale) in [
-        # Eh, the name 'AspectList_WeaponUseModeSecondaryAspectData' doesn't exist in this object, which I think is the main
-        # thing screwing up altering this.
-        #('Pistol: Atomizer',
-        #    '/Game/Gear/Weapons/Pistols/Maliwan/_Shared/_Design/Parts/Barrel/Barrel_01/Part_PS_MAL_Barrel_01.Part_PS_MAL_Barrel_01',
-        #    2,
-        #    0.5,
-        #    scale_pistol),
+for (label, obj_name, aspect_num, default, scale, aspect_obj) in [
+        ('Pistol: Atomizer',
+            '/Game/Gear/Weapons/Pistols/Maliwan/_Shared/_Design/Parts/Barrel/Barrel_01/Part_PS_MAL_Barrel_01',
+            2,
+            0.5,
+            scale_pistol,
+            'WeaponUseModeSecondaryAspectData_0'),
+        ('Pistol: Melter',
+            '/Game/Gear/Weapons/Pistols/Maliwan/_Shared/_Design/Parts/Barrel/Barrel_02/Part_PS_MAL_Barrel_02',
+            2,
+            1,
+            scale_pistol,
+            'WeaponUseModeSecondaryAspectData_1'),
         ('Pistol: Blaster',
             '/Game/Gear/Weapons/Pistols/Maliwan/_Shared/_Design/Parts/Barrel/Barrel_03/Part_PS_MAL_Barrel_03.Part_PS_MAL_Barrel_03',
             0,
             0.7,
-            scale_pistol),
+            scale_pistol,
+            None),
         ('Shotgun: Terminator',
             '/Game/Gear/Weapons/Shotguns/Maliwan/_Shared/_Design/Parts/Barrel/Barrel_01/Part_SG_MAL_Barrel_01.Part_SG_MAL_Barrel_01',
             1,
             0.7,
-            scale_shotgun),
+            scale_shotgun,
+            None),
         ('Shotgun: Shockwave',
             '/Game/Gear/Weapons/Shotguns/Maliwan/_Shared/_Design/Parts/Barrel/Barrel_02/Part_SG_MAL_Barrel_02.Part_SG_MAL_Barrel_02',
             1,
             1.05,
-            scale_shotgun),
+            scale_shotgun,
+            None),
         ('Shotgun: Cyclotron',
             '/Game/Gear/Weapons/Shotguns/Maliwan/_Shared/_Design/Parts/Barrel/Barrel_03/Part_SG_MAL_Barrel_03.Part_SG_MAL_Barrel_03',
             1,
             0.9,
-            scale_shotgun),
+            scale_shotgun,
+            None),
         ('SMG: Pulsar',
             '/Game/Gear/Weapons/SMGs/Maliwan/_Shared/_Design/Parts/Barrel/Barrel_01/Part_SM_MAL_Barrel_01.Part_SM_MAL_Barrel_01',
             1,
             0.75,
-            scale_smg),
+            scale_smg,
+            None),
         ('SMG: Nebula',
             '/Game/Gear/Weapons/SMGs/Maliwan/_Shared/_Design/Parts/Barrel/Barrel_02/Part_SM_MAL_Barrel_02.Part_SM_MAL_Barrel_02',
             1,
             0.85,
-            scale_smg),
+            scale_smg,
+            None),
         ('SMG: Quasar',
             '/Game/Gear/Weapons/SMGs/Maliwan/_Shared/_Design/Parts/Barrel/Barrel_03/Part_SM_MAL_Barrel_03.Part_SM_MAL_Barrel_03',
             1,
             1.0,
-            scale_smg),
+            scale_smg,
+            None),
         ('Sniper Rifle: Multiplex',
             '/Game/Gear/Weapons/SniperRifles/Maliwan/Shared/_Design/Parts/Barrel/Barrel_01/Part_MAL_SR_Barrel_01.Part_MAL_SR_Barrel_01',
             1,
             1.1,
-            scale_sniper),
+            scale_sniper,
+            None),
         ('Sniper Rifle: Particle Rifle',
             '/Game/Gear/Weapons/SniperRifles/Maliwan/Shared/_Design/Parts/Barrel/Barrel_02/Part_MAL_SR_Barrel_02.Part_MAL_SR_Barrel_02',
             1,
             0.9,
-            scale_sniper),
+            scale_sniper,
+            None),
         ('Sniper Rifle: Proton Rifle',
             '/Game/Gear/Weapons/SniperRifles/Maliwan/Shared/_Design/Parts/Barrel/Barrel_03/Part_MAL_SR_Barrel_03.Part_MAL_SR_Barrel_03',
             1,
             1.75,
-            scale_sniper),
+            scale_sniper,
+            None),
         ]:
+
+    if not aspect_obj:
+        aspect_obj = 'AspectList_WeaponUseModeSecondaryAspectData'
+
     mod.comment('{} (default: {})'.format(label, default))
-    charge_time(mod, obj_name, default*scale, aspect_list=aspect_num)
+    charge_time(mod, obj_name, default*scale,
+            aspect_obj=aspect_obj,
+            aspect_list=aspect_num,
+            )
     mod.newline()
 
 mod.header('Legendaries/Uniques')
@@ -201,9 +217,7 @@ for label, obj_name, default, scale, aspect_obj, aspect_num, aspect_attr, final_
             None),
         ('Complex Root',
             '/Game/PatchDLC/Geranium/Gear/Weapon/_Unique/ImaginaryNumber/Parts/Part_MAL_SR_Barrel_ImaginaryNumber',
-            # Not sure what the *actual* value is -- it's the default number, and isn't actually present in the
-            # object serializations.  Since we convert snipers to 0, though, it sort of doesn't matter, and
-            # this does happen to work.
+            # The default `ChargeTime` is 1, which is why this value doesn't show up in serializations.
             1,
             scale_sniper,
             None,
@@ -226,14 +240,16 @@ for label, obj_name, default, scale, aspect_obj, aspect_num, aspect_attr, final_
             None,
             None,
             None),
-        # This *does* change the attribute we intend to change, but has no effect on the actual charge
-        # time.  Which might make sense given that this is 2 but the charge time seems closer to 1.
-        #('D.N.A.',
-        #    '/Game/PatchDLC/Mayhem2/Gear/Weapon/_Shared/_Unique/DNA/Parts/Part_SM_MAL_Barrel_DNA.Part_SM_MAL_Barrel_DNA',
-        #    2.0,
-        #    scale_smg,
-        #    None,
-        #    'DischargeTime'),
+        ('D.N.A.',
+            '/Game/PatchDLC/Mayhem2/Gear/Weapon/_Shared/_Unique/DNA/Parts/Part_SM_MAL_Barrel_DNA',
+            # The `ChargeTime` attr is 1.0, which won't show up in serializations 'cause that's the default.
+            # You can see it with `getall`, though.
+            1.0,
+            scale_smg,
+            None,
+            3,
+            None,
+            None),
         ('Destructo Spinner',
             '/Game/Gear/Weapons/SMGs/Maliwan/_Shared/_Design/_Unique/DestructoSpin/Part/Part_SM_MAL_Barrel_DestructoSpin.Part_SM_MAL_Barrel_DestructoSpin',
             0.5,
@@ -282,6 +298,16 @@ for label, obj_name, default, scale, aspect_obj, aspect_num, aspect_attr, final_
             None,
             None,
             None),
+        ('Free Radical',
+            '/Game/PatchDLC/Ixora2/Gear/Weapons/_Unique/Deatomizer/Parts/Part_PS_MAL_Barrel_Deatomizer2',
+            # Okay, fun!  When the `ChargeTime` attr isn't in the serializations, it's 'cause it's
+            # set to the default of 1.0.  You can see it with a `getall`.
+            1.0,
+            scale_pistol,
+            None,
+            0,
+            None,
+            None),
         ('Frequency',
             '/Game/PatchDLC/Geranium/Gear/Weapon/_Unique/Frequency/Parts/Part_SG_MAL_Barrel_Frequency',
             1.05,
@@ -296,6 +322,14 @@ for label, obj_name, default, scale, aspect_obj, aspect_num, aspect_attr, final_
             scale_pistol,
             None,
             None,
+            None,
+            None),
+        ('Ice Pick',
+            '/Game/PatchDLC/Event2/Gear/Weapon/_Unique/IcePick/Parts/Part_PS_MAL_Barrel_IcePick',
+            1,
+            scale_pistol,
+            'WeaponUseModeSecondaryAspectData_1',
+            2,
             None,
             None),
         ('ION LASER',
@@ -378,6 +412,14 @@ for label, obj_name, default, scale, aspect_obj, aspect_num, aspect_attr, final_
             None,
             None,
             None),
+        ('Starkiller',
+            '/Game/Gear/Weapons/Pistols/Maliwan/_Shared/_Design/_Unique/Starkiller/Parts/Part_PS_MAL_Barrel_Starkiller',
+            1,
+            scale_pistol,
+            'WeaponUseModeSecondaryAspectData_1',
+            2,
+            None,
+            None),
         ('Storm',
             '/Game/Gear/Weapons/SniperRifles/Maliwan/Shared/_Design/_Unique/_Legendary/Storm/Parts/Part_MAL_SR_Barrel_Storm.Part_MAL_SR_Barrel_Storm',
             1.0, # pre-GBX-hotfix value: 2.0
@@ -444,126 +486,6 @@ for label, obj_name, default, scale, aspect_obj, aspect_num, aspect_attr, final_
 #        ]:
 #    mod.comment('{} (default: {})'.format(label, default))
 #    charge_time(mod, obj_name, default*scale, aspect_list=aspect_idx, aspect_attr=aspect_attr)
-#    mod.newline()
-
-# So, pistols.  Every other maliwan gun's .uasset file has the name "AspectList_WeaponUseModeSecondaryAspectData"
-# defined, which is used after the colon in the object name to reference things properly - you start with the
-# AspectList, head to the WeaponUseModeSecondaryAspectData, and get to the Component from there.  Atomizers,
-# Melters, and the Starkiller don't actually have that name, though, and I couldn't tell you how to get to the
-# right object without it, though I've tried a bunch of different ways.  So, eh.  Perhaps if I ever get a console
-# with at least `getall` I could get a better feel for it and find a syntax that works, but I'm continuing to
-# just give up for now.
-#mod.comment('Pistol Tests...')
-#for obj_name, new_val in [
-#        ('/Game/Gear/Weapons/Pistols/Maliwan/_Shared/_Design/Parts/Barrel/Barrel_01/Part_PS_MAL_Barrel_01.Part_PS_MAL_Barrel_01', 5),
-#        ('/Game/Gear/Weapons/Pistols/Maliwan/_Shared/_Design/Parts/Barrel/Barrel_02/Part_PS_MAL_Barrel_02.Part_PS_MAL_Barrel_02', 10),
-#        ]:
-#    charge_time(mod, obj_name, new_val)
-#mod.newline()
-
-# Read below for a very labor-intensive way to glean the default values, in the
-# absence of an in-game console, or other introspection methods!  It's... fun?
-#
-# (no, it's not, really)
-#
-# This method is quite obsolete now that we've discovered JohnWickParse, which
-# doesn't serialize *all* the Barrel part data, but does serialize enough that
-# you can get the value right from the JSON file.  Leaving it in just out of
-# some weird nostalgia, but JWP is the way to go.
-
-# Figuring out defaults by basically doing set_cmp on a wide range of possible values,
-# setting the new value to some obviously-distinct times.  I used 3-second intervals
-# so that any timing errors or whatever would be entirely negligible.  I'd done
-# initial manual times for the vanilla guns, so I had a pretty reasonable idea about
-# the start range to try, though in the end I had to do a second pass to widen up
-# the "from" values a bit more, for half of 'em.
-#for (label, part, start) in [
-#        # First pass...
-#        #('pistol3', '/Game/Gear/Weapons/Pistols/Maliwan/_Shared/_Design/Parts/Barrel/Barrel_03/Part_PS_MAL_Barrel_03.Part_PS_MAL_Barrel_03', 0.5),
-#        #('shotty1', '/Game/Gear/Weapons/Shotguns/Maliwan/_Shared/_Design/Parts/Barrel/Barrel_01/Part_SG_MAL_Barrel_01.Part_SG_MAL_Barrel_01', 0.9),
-#        #('shotty2', '/Game/Gear/Weapons/Shotguns/Maliwan/_Shared/_Design/Parts/Barrel/Barrel_02/Part_SG_MAL_Barrel_02.Part_SG_MAL_Barrel_02', 1.15),
-#        #('shotty3', '/Game/Gear/Weapons/Shotguns/Maliwan/_Shared/_Design/Parts/Barrel/Barrel_03/Part_SG_MAL_Barrel_03.Part_SG_MAL_Barrel_03', 1.0),
-#        #('smg1', '/Game/Gear/Weapons/SMGs/Maliwan/_Shared/_Design/Parts/Barrel/Barrel_01/Part_SM_MAL_Barrel_01.Part_SM_MAL_Barrel_01', 0.7),
-#        #('smg2', '/Game/Gear/Weapons/SMGs/Maliwan/_Shared/_Design/Parts/Barrel/Barrel_02/Part_SM_MAL_Barrel_02.Part_SM_MAL_Barrel_02', 0.9),
-#        #('smg3', '/Game/Gear/Weapons/SMGs/Maliwan/_Shared/_Design/Parts/Barrel/Barrel_03/Part_SM_MAL_Barrel_03.Part_SM_MAL_Barrel_03', 0.9),
-#        #('sniper1', '/Game/Gear/Weapons/SniperRifles/Maliwan/Shared/_Design/Parts/Barrel/Barrel_01/Part_MAL_SR_Barrel_01.Part_MAL_SR_Barrel_01', 1.2),
-#        #('sniper2', '/Game/Gear/Weapons/SniperRifles/Maliwan/Shared/_Design/Parts/Barrel/Barrel_02/Part_MAL_SR_Barrel_02.Part_MAL_SR_Barrel_02', 0.8),
-#        #('sniper3', '/Game/Gear/Weapons/SniperRifles/Maliwan/Shared/_Design/Parts/Barrel/Barrel_03/Part_MAL_SR_Barrel_03.Part_MAL_SR_Barrel_03', 1.7),
-#
-#        # Second pass... (got 'em!)
-#        #('shotty1', '/Game/Gear/Weapons/Shotguns/Maliwan/_Shared/_Design/Parts/Barrel/Barrel_01/Part_SG_MAL_Barrel_01.Part_SG_MAL_Barrel_01', 0.7),
-#        #('shotty2', '/Game/Gear/Weapons/Shotguns/Maliwan/_Shared/_Design/Parts/Barrel/Barrel_02/Part_SG_MAL_Barrel_02.Part_SG_MAL_Barrel_02', 1.0),
-#        #('shotty3', '/Game/Gear/Weapons/Shotguns/Maliwan/_Shared/_Design/Parts/Barrel/Barrel_03/Part_SG_MAL_Barrel_03.Part_SG_MAL_Barrel_03', 0.8),
-#        #('smg2', '/Game/Gear/Weapons/SMGs/Maliwan/_Shared/_Design/Parts/Barrel/Barrel_02/Part_SM_MAL_Barrel_02.Part_SM_MAL_Barrel_02', 0.7),
-#        #('sniper1', '/Game/Gear/Weapons/SniperRifles/Maliwan/Shared/_Design/Parts/Barrel/Barrel_01/Part_MAL_SR_Barrel_01.Part_MAL_SR_Barrel_01', 1.0),
-#
-#        # Legendaries/Uniques pass 1
-#        #('Cloud Kill',
-#        #    '/Game/Gear/Weapons/SMGs/Maliwan/_Shared/_Design/_Unique/CloudKill/Parts/Part_SM_MAL_Barrel_CloudKill.Part_SM_MAL_Barrel_CloudKill', 0.85),
-#        #('Crit',
-#        #    '/Game/Gear/Weapons/SMGs/Maliwan/_Shared/_Design/_Unique/Crit/Parts/Part_SM_MAL_Barrel_Crit.Part_SM_MAL_Barrel_Crit', 0.70),
-#        #('Cutsman',
-#        #    '/Game/Gear/Weapons/SMGs/Maliwan/_Shared/_Design/_Unique/Cutsman/Parts/Part_SM_MAL_Barrel_Cutsman.Part_SM_MAL_Barrel_Cutsman', 1.0),
-#        #('Destructo Spinner',
-#        #    '/Game/Gear/Weapons/SMGs/Maliwan/_Shared/_Design/_Unique/DestructoSpin/Part/Part_SM_MAL_Barrel_DestructoSpin.Part_SM_MAL_Barrel_DestructoSpin', 0.5),
-#        #('E-Gone',
-#        #    '/Game/Gear/Weapons/SMGs/Maliwan/_Shared/_Design/_Unique/Egon/Parts/Part_SM_MAL_Barrel_Egon.Part_SM_MAL_Barrel_Egon', 0.70),
-#        #("The Emperor's Condiment",
-#        #    '/Game/Gear/Weapons/SMGs/Maliwan/_Shared/_Design/_Unique/Emporer/Parts/Part_SM_MAL_Barrel_Emporer.Part_SM_MAL_Barrel_Emporer', 0.8),
-#        #('Hellshock',
-#        #    '/Game/Gear/Weapons/Pistols/Maliwan/_Shared/_Design/_Unique/Hellshock/Parts/Part_PS_MAL_Barrel_HellShock.Part_PS_MAL_Barrel_HellShock', 1.15),
-#        #("Kill-o'-the-Wisp",
-#        #    '/Game/Gear/Weapons/Shotguns/Maliwan/_Shared/_Design/_Unique/Wisp/Parts/Part_SG_MAL_Barrel_Wisp.Part_SG_MAL_Barrel_Wisp', 1.15),
-#        #('Krakatoa',
-#        #    '/Game/Gear/Weapons/SniperRifles/Maliwan/Shared/_Design/_Unique/_Legendary/Krakatoa/Parts/Part_MAL_SR_Barrel_Krakatoa.Part_MAL_SR_Barrel_Krakatoa', 0.8),
-#        #("Miss Moxxi's Vibra-Pulse",
-#        #    '/Game/Gear/Weapons/SMGs/Maliwan/_Shared/_Design/_Unique/VibraPulse/Parts/Part_SM_MAL_Barrel_VibraPulse.Part_SM_MAL_Barrel_VibraPulse', 1.0),
-#        #('Projectile Recursion',
-#        #    '/Game/Gear/Weapons/Shotguns/Maliwan/_Shared/_Design/_Unique/Recursion/Parts/Part_SG_MAL_Barrel_Recursion.Part_SG_MAL_Barrel_Recursion', 0.9),
-#        #('Shrieking Devil',
-#        #    '/Game/Gear/Weapons/Shotguns/Maliwan/_Shared/_Design/_Unique/Shriek/Parts/Part_SG_MAL_Barrel_Shriek.Part_SG_MAL_Barrel_Shriek', 1.95),
-#        #('Soleki Protocol',
-#        #    '/Game/Gear/Weapons/SniperRifles/Maliwan/Shared/_Design/_Unique/_Legendary/Soleki/Parts/Part_MAL_SR_Barrel_Soleki.Part_MAL_SR_Barrel_Soleki', 1.2),
-#        #('Tsunami',
-#        #    '/Game/Gear/Weapons/SMGs/Maliwan/_Shared/_Design/_Unique/Tsunami/Parts/Part_SM_MAL_Barrel_Tsunami.Part_SM_MAL_Barrel_Tsunami', 0.75),
-#
-#        # Legendaries/Uniques pass 2
-#        #('Hellshock',
-#        #    '/Game/Gear/Weapons/Pistols/Maliwan/_Shared/_Design/_Unique/Hellshock/Parts/Part_PS_MAL_Barrel_HellShock.Part_PS_MAL_Barrel_HellShock', 0.85),
-#        #('Cloud Kill',
-#        #    '/Game/Gear/Weapons/SMGs/Maliwan/_Shared/_Design/_Unique/CloudKill/Parts/Part_SM_MAL_Barrel_CloudKill.Part_SM_MAL_Barrel_CloudKill', 0.7),
-#        #("Kill-o'-the-Wisp",
-#        #    '/Game/Gear/Weapons/Shotguns/Maliwan/_Shared/_Design/_Unique/Wisp/Parts/Part_SG_MAL_Barrel_Wisp.Part_SG_MAL_Barrel_Wisp', 0.95),
-#
-#        # Legendaries/Uniques pass 3
-#        #('Hellshock',
-#        #    '/Game/Gear/Weapons/Pistols/Maliwan/_Shared/_Design/_Unique/Hellshock/Parts/Part_PS_MAL_Barrel_HellShock.Part_PS_MAL_Barrel_HellShock', 0.7),
-#
-#        ### Maliwan Takedown / Mayhem 4 Gear
-#        #("Kyb's Worth",
-#        #    '/Game/PatchDLC/Raid1/Gear/Weapons/KybsWorth/Parts/Part_SM_MAL_Barrel_KybsWorth.Part_SM_MAL_Barrel_KybsWorth', 0.6),
-#        #('P2P Networker',
-#        #    '/Game/PatchDLC/Raid1/Gear/Weapons/Link/Parts/Part_SM_MAL_Barrel_Link.Part_SM_MAL_Barrel_Link', 0.8),
-#        #("Vosk's Deathgrip",
-#        #    '/Game/PatchDLC/Raid1/Re-Engagement/Weapons/DeathGrip/Parts/Part_SG_MAL_Barrel_DeathGrip.Part_SG_MAL_Barrel_DeathGrip', 0.5),
-#
-#        # Take 2.
-#        ("Vosk's Deathgrip",
-#            '/Game/PatchDLC/Raid1/Re-Engagement/Weapons/DeathGrip/Parts/Part_SG_MAL_Barrel_DeathGrip.Part_SG_MAL_Barrel_DeathGrip', 0.8),
-#        ]:
-#    mod.comment(label)
-#    prev_val = start
-#    new_val = 3
-#    steps = 0
-#    while steps < 7:
-#        charge_time(mod,
-#                part,
-#                new_val,
-#                prev_val=prev_val,
-#                )
-#        steps += 1
-#        new_val += 3
-#        prev_val += 0.05
 #    mod.newline()
 
 mod.close()
